45ecb9624255a1435391d98d9640e63537a4cb26
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-11-07 22:16:14 +0100

    bench updates

--- src/bin/bench.rs
+++ src/bin/bench.rs
@@ -521,12 +521,33 @@ fn bench_best(seq: &[u8], queries: &QS) {
 }
 
 #[inline(never)]
+fn bench_rank9(seq: &[u8], queries: &QS) {
+    eprint!("{:<20}:", "rank9");
+
+    // Cast to slice of usize.
+    let bitvec = unsafe { BitVec::from_raw_parts(seq.align_to().1, seq.len() * 8) };
+    let rank9 = sux::rank_sel::Rank9::new(bitvec);
+
+    let bits = rank9.mem_size(Default::default()) as f64 / seq.len() as f64;
+    eprint!("{bits:>6.2}b |");
+
+    time(&queries, |p| [rank9.rank(p) as u32, 0, 0, 0]);
+    eprintln!();
+}
+
+#[inline(never)]
 fn bench_quart<const C3: bool>(seq: &[u8], queries: &QS) {
     eprint!("{:<20}:", format!("QuartBlock {C3}"));
     let bits = 4.0;
     eprint!("{bits:>6.2}b |");
 
     let ranker = Ranker::<QuartBlock>::new(&seq);
+
+    // time_fn(queries, |queries| {
+    //     for &q in queries {
+    //         std::hint::black_box(ranker.count1(q, q as u8 & 3));
+    //     }
+    // });
 
     time(&queries, |p| ranker.count::<count4::U64Popcnt, C3>(p));
 
@@ -548,7 +569,7 @@ fn bench_quart<const C3: bool>(seq: &[u8], queries: &QS) {
         &queries,
         B,
         |p| ranker.prefetch(p),
-        |p| ranker.count::<count4::SimdCount, C3>(p),
+        |p| ranker.count::<count4::SimdCount, false>(p),
     );
 
     eprint!(" |");
@@ -560,29 +581,14 @@ fn bench_quart<const C3: bool>(seq: &[u8], queries: &QS) {
         ranker.count_coro::<count4::ByteLookup8, C3>(p)
     });
     time_coro_stream(&queries, B, |p| {
-        ranker.count_coro::<count4::SimdCount, C3>(p)
+        ranker.count_coro::<count4::SimdCount, false>(p)
     });
-    eprintln!();
-}
-
-#[inline(never)]
-fn bench_rank9(seq: &[u8], queries: &QS) {
-    eprint!("{:<20}:", "rank9");
-
-    // Cast to slice of usize.
-    let bitvec = unsafe { BitVec::from_raw_parts(seq.align_to().1, seq.len() * 8) };
-    let rank9 = sux::rank_sel::Rank9::new(bitvec);
-
-    let bits = rank9.mem_size(Default::default()) as f64 / seq.len() as f64;
-    eprint!("{bits:>6.2}b |");
-
-    time(&queries, |p| [rank9.rank(p) as u32, 0, 0, 0]);
     eprintln!();
 }
 
 fn main() {
     #[cfg(debug_assertions)]
-    let q = 100_000;
+    let q = 10_000;
     #[cfg(debug_assertions)]
     let ns = [100_000];
     #[cfg(not(debug_assertions))]
@@ -600,10 +606,10 @@ fn main() {
                 .collect::<Vec<_>>()
         });
 
-        bench_rank9(&seq, &queries);
         bench_quart::<true>(&seq, &queries);
         bench_quart::<false>(&seq, &queries);
         bench_best(&seq, &queries);
+        // bench_rank9(&seq, &queries);
 
         // bench_bwa4_rank(&seq, &queries);
         // bench_bwa3_rank(&seq, &queries);
