89f33a3b8468118e3481bc8e5742f105ae639222
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-11-13 17:16:42 +0100

    add plot to readme

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ queries over a 2-bit DNA alphabet.
 The main API looks like this:
 
 ``` rust
-impl DnaRank {
+impl QuadRank {
     /// Take a DNA string over ACGT characters.
     pub fn new(seq: &[u8]) -> Self;
     
@@ -14,3 +14,21 @@ impl DnaRank {
     pub fn count4(&self, pos: usize) -> [u32; 4];
 }
 ```
+
+## Small FM-index evaluation 
+In `mapper/` there is a count-only FM-index implementation using `QuadRank`.
+
+Here I'm mapping simulated 150bp short reads with 1% error rate against 550MB of
+viral genomes. I first build each index (where I don't care about time/space
+usage), and then count the number of matches of each fwd/rc read.
+For `genedex` and `quad`, I query batches of 32 reads at a time.
+All results are multithreaded (12 threads, on my 6-core i7-10750H) via `rayon::par_iter`.
+
+- `Quad<>`: High-level rank structure: `src/ranker.rs`.
+- `QuartBlock`: simple-but-large 512bit basic block for rank.
+- `HexBlock`: small-but-intricate 512bit basic block that's a bit slower.
+- `fm_index`: https://github.com/ajalab/fm-index
+- `genedex`: https://github.com/feldroop/genedex, supports batching
+- `RSQVector256`: Rank structure of https://github.com/rossanoventurini/qwt
+
+![Comparison plot, showing that quadrank smaller and faster than others.](mapper/comparison.png)
--- mapper/plot.py
+++ mapper/plot.py
@@ -8,28 +8,41 @@
 # make a log-log plot
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
 
 data = pd.read_csv(
     "data.csv",
-    names=["name", "size", "speed", "color"],
-    dtype={"name": str, "size": float, "speed": float, "color": str},
+    names=["name", "size", "speed", "color", "lookup"],
+    dtype={"name": str, "size": float, "speed": float, "color": str, "lookup": int},
     header=0,
 )
-print(data)
+# multiple speed by 1000
+data["speed"] /= 1000
+
 plt.figure(figsize=(7, 4))
-for k, group in data.groupby("color"):
+for i, row in data.iterrows():
     plt.semilogy(
-        group["size"],
-        group["speed"],
-        marker="o",
-        ms=12,
+        row["size"],
+        row["speed"],
+        marker="o" if row["lookup"] == 0 else "s",
+        ms=12 if row["lookup"] == 0 else 10,
         linestyle="None",
-        label=k,
-        color=k,
+        label=row["name"],
+        color=row["color"],
         base=2,
     )
+formatter = plt.ScalarFormatter()
+plt.gca().yaxis.set_major_formatter(formatter)
+
+# set y-ax labels
+plt.yticks([0.5, 1, 2, 4, 8])
+plt.ylim(0.4, 12)
+
+plt.xlim(left=0)
+plt.axvline(x=2.0, color="red", linestyle="-", linewidth=1)
+
 plt.xlabel("Size (bits/bp)")
-plt.ylabel("Speed (k reads/s)")
+plt.ylabel("Speed (M reads/s)")
 plt.title("FM-index exact read mapping size and throughput")
 plt.grid(True, which="both", ls="--")
 for i, row in data.iterrows():
