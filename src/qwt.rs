4ccac2e92bd6a08e1a6b98675560c1c2f612150c
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2025-12-18 22:44:41 +0100

    update qwt for MemSize implementation

--- src/qwt.rs
+++ src/qwt.rs
@@ -1,4 +1,5 @@
-use qwt::{RSQVector256, RankQuad, SpaceUsage, WTSupport};
+use packed_seq::{PackedSeqVec, SeqVec};
+use qwt::{RSQVector256, RankBin, RankQuad, WTSupport};
 
 use crate::ranker::RankerT;
 
@@ -20,7 +21,7 @@ impl RankerT for RSQVector256 {
     }
 
     fn size(&self) -> usize {
-        self.space_usage_byte()
+        mem_dbg::MemSize::mem_size(self, Default::default())
     }
 
     #[inline(always)]
@@ -31,5 +32,77 @@ impl RankerT for RSQVector256 {
     #[inline(always)]
     fn count1(&self, pos: usize, c: u8) -> u32 {
         unsafe { self.rank_unchecked(c, pos) as u32 }
+    }
+}
+
+impl RankerT for qwt::RSNarrow {
+    fn new(seq: &[u8]) -> Self {
+        let packed_seq = PackedSeqVec::from_ascii(seq).into_raw();
+        let iter = packed_seq
+            .as_chunks()
+            .0
+            .iter()
+            .map(|x| u64::from_le_bytes(*x));
+        let mut bitvec = qwt::BitVectorMut::default();
+        for x in iter {
+            bitvec.append_bits(x, 64);
+        }
+
+        Self::new(bitvec.into())
+    }
+
+    #[inline(always)]
+    fn prefetch(&self, pos: usize) {
+        RankBin::prefetch(self, pos);
+    }
+
+    fn size(&self) -> usize {
+        mem_dbg::MemSize::mem_size(self, Default::default())
+    }
+
+    #[inline(always)]
+    fn count(&self, _pos: usize) -> crate::Ranks {
+        unimplemented!()
+    }
+
+    #[inline(always)]
+    fn count1(&self, pos: usize, _c: u8) -> u32 {
+        unsafe { self.rank1_unchecked(pos) as u32 }
+    }
+}
+
+impl RankerT for qwt::RSWide {
+    fn new(seq: &[u8]) -> Self {
+        let packed_seq = PackedSeqVec::from_ascii(seq).into_raw();
+        let iter = packed_seq
+            .as_chunks()
+            .0
+            .iter()
+            .map(|x| u64::from_le_bytes(*x));
+        let mut bitvec = qwt::BitVectorMut::default();
+        for x in iter {
+            bitvec.append_bits(x, 64);
+        }
+
+        Self::new(bitvec.into())
+    }
+
+    #[inline(always)]
+    fn prefetch(&self, pos: usize) {
+        RankBin::prefetch(self, pos);
+    }
+
+    fn size(&self) -> usize {
+        mem_dbg::MemSize::mem_size(self, Default::default())
+    }
+
+    #[inline(always)]
+    fn count(&self, _pos: usize) -> crate::Ranks {
+        unimplemented!()
+    }
+
+    #[inline(always)]
+    fn count1(&self, pos: usize, _c: u8) -> u32 {
+        unsafe { self.rank1_unchecked(pos) as u32 }
     }
 }
