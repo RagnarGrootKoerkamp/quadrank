f92e61c3b44e474764373ba6a703480ba1924e25
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-01-17 12:08:36 +0100

    [block] HalfBlock can safely have SHIFT=11 rather than 8

--- src/binary/super_blocks.rs
+++ src/binary/super_blocks.rs
@@ -16,29 +16,31 @@ impl SuperBlock for TrivialSB {
     }
 }
 
-/// Store the high 32 bits of `x>>8`.
+/// Store the high 32 bits of `x>>11`.
 #[derive(mem_dbg::MemSize)]
 pub struct HalfSB {
     rank: u32,
 }
 
+const SHIFT: usize = 11;
+
 impl SuperBlock for HalfSB {
     #[inline(always)]
     fn new(rank: u64) -> (Self, u64) {
-        let rank = rank >> 8;
+        let rank = rank >> SHIFT;
         assert!(
             rank <= u32::MAX as u64,
             "Rank too large for HalfSB. Use TrivialSB instead."
         );
         (
             Self {
-                rank: (rank) as u32,
+                rank: rank.try_into().unwrap(),
             },
-            rank << 8,
+            rank << SHIFT,
         )
     }
     #[inline(always)]
     fn get(&self) -> u64 {
-        (self.rank as u64) << 8
+        (self.rank as u64) << SHIFT
     }
 }
