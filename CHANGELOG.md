14d23d782e9f5fca8f3045d6cdf95a343c069887
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2026-02-03 20:04:59 +0100

    changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@
 
 ## git
 
+- Now works on stable rust!
+  - portable_simd => wide
+  - refactors/simplifications to drop `const_generics` requirement
+- API cleanup (make some stuff private; expose some type aliases)
+- Make `RankerT::rank` method unsafe and rename to `rank_unchecked`.
+
 ## 0.1.0
 
 - Initial release.
--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,14 @@ pub mod ext {
 }
 
 // Type aliases
+/// Main binary rank structure. Alias for [`BiRank16`].
 pub type BiRank = BiRank16;
 pub type BiRank16 = binary::Ranker<binary::blocks::BinaryBlock16>;
 pub type BiRank16x2 = binary::Ranker<binary::blocks::BinaryBlock16x2>;
 pub type BiRank32x2 = binary::Ranker<binary::blocks::BinaryBlock32x2>;
 pub type BiRank64x2 = binary::Ranker<binary::blocks::BinaryBlock64x2>;
 
+/// Main DNA rank structure. Alias for [`QuadRank16`].
 pub type QuadRank = QuadRank16;
 pub type QuadRank16 = quad::Ranker<quad::blocks::QuadBlock16>;
 pub type QuadRank24_8 = quad::Ranker<quad::blocks::QuadBlock24_8>;
